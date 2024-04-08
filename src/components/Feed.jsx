09fa248d64284de9f88163b605146e66a808ff73
mahesh <jadhao.mahesh1009@gmail.com>
2024-04-08 18:44:08 +0530

    complite mini profile component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,12 +6,15 @@ function Feed() {
   return (
     <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
       {/* Posts (left) */}
-      <section className='md:cols-span-2'>
+      <section className='md:col-span-2'>
         <Posts/>
       </section>
       {/* MiniProfile (right) */}
       <section className='hidden md:inline-grid md:col-span-1'>
-        <MiniProfile/>
+        <div className="fixed w-[380px]">
+         <MiniProfile/>
+        </div>
+        
       </section>
     </main>
   )
--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+/* eslint-disable @next/next/no-img-element */
+'use client'
+
+import { useSession , signIn , signOut } from "next-auth/react"
 
 function MiniProfile() {
+    const { data:session } = useSession();
+
   return (
-    <div>
-      MIniProfile
+    <div className="flex items-center justify-between mt-14 scroll-ml-10">
+        <img src={
+            session?.user?.image || "/800px-Instagram_logo_2016.webp"
+            
+        }
+        alt="Instagram"
+        className="w-16 h-16 rounded-full border p-[2px]"
+        />
+        <div className="flex-1 ml-4">
+            <h2 className="font-bold">{session?.user?.username}</h2>
+            <h3 className="text-sm text-gray-400">Welcome to Instragram</h3>
+        </div>
+        {
+            session ? (
+                <button
+                  onClick={signOut}
+                  className="text-blue-500 text-sm font-semibold"
+                >
+                    Sign Out
+                </button>
+            ):(
+                <button
+                onClick={signIn}
+                className="text-blue-500 text-sm font-semibold"
+              >
+                  Sign In
+              </button>
+            )
+        }
     </div>
   )
 }
